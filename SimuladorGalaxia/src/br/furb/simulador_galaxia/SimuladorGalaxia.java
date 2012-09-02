d355b884e3f1f9ceee4146af943415967a20614b
Andre Ramaciotti <andre@ramaciotti.com>
2012-09-02 12:48:27 -0300

    Botando o JOGL para funcionar

--- SimuladorGalaxia/src/br/furb/simulador_galaxia/SimuladorGalaxia.java
+++ SimuladorGalaxia/src/br/furb/simulador_galaxia/SimuladorGalaxia.java
@@ -1,10 +1,27 @@
 package br.furb.simulador_galaxia;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.media.opengl.GLCanvas;
+import javax.swing.JFrame;
+
 public class SimuladorGalaxia {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		GLCanvas canvas = new GLCanvas();
+		canvas.setSize(600, 400);
 
+		JFrame frame = new JFrame("Simulador Galáxia");
+		frame.getContentPane().add(canvas);
+		frame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent ev) {
+				System.exit(0);
+			}
+		});
+
+		frame.setSize(frame.getContentPane().getPreferredSize());
+		frame.setVisible(true);
 	}
 
 }
--- SimuladorGalaxia/src/br/furb/simulador_galaxia/Tela.java
+++ SimuladorGalaxia/src/br/furb/simulador_galaxia/Tela.java
@@ -2,6 +2,11 @@ package br.furb.simulador_galaxia;
 
 import java.util.concurrent.locks.Lock;
 
+// Pro import abaixo funcionar, é necessário que o JOGL esteja instalado. Acho
+// melhor instalá-lo no sistema em vez de colocá-lo diretamente no projeto por
+// causa de seu tamanho.
+import javax.media.opengl.*;
+
 public class Tela {
 	private PontoOrtogonal[] pontos;
 	private Lock lock;
