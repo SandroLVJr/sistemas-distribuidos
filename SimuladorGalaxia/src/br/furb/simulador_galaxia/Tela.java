e3d8b2bb8fd0176042501b35f8fbc91b2eda415d
Andre Ramaciotti <andre@ramaciotti.com>
2012-09-02 16:04:08 -0300

    Desenhar VertexArray

--- SimuladorGalaxia/src/br/furb/simulador_galaxia/Tela.java
+++ SimuladorGalaxia/src/br/furb/simulador_galaxia/Tela.java
@@ -1,5 +1,6 @@
 package br.furb.simulador_galaxia;
 
+import java.nio.FloatBuffer;
 import java.util.concurrent.locks.Lock;
 
 import javax.media.opengl.DebugGL;
@@ -10,6 +11,7 @@ import javax.media.opengl.GLCapabilities;
 import javax.media.opengl.GLEventListener;
 import javax.media.opengl.glu.GLU;
 
+import com.sun.opengl.util.BufferUtil;
 import com.sun.opengl.util.FPSAnimator;
 
 public class Tela extends GLCanvas implements GLEventListener {
@@ -27,35 +29,45 @@ public class Tela extends GLCanvas implements GLEventListener {
 		setSize(width, height);
 		addGLEventListener(this);
 	}
-	
+
 	private void setCamera(GL gl) {
 		gl.glMatrixMode(GL.GL_PROJECTION);
-        gl.glLoadIdentity();
+		gl.glLoadIdentity();
 
-        float widthHeightRatio = (float) getWidth() / (float) getHeight();
-        glu.gluPerspective(45, widthHeightRatio, 1, 1000);
-        glu.gluLookAt(0, 0, 100, 0, 0, 0, 0, 1, 0);
+		float widthHeightRatio = (float) getWidth() / (float) getHeight();
+		glu.gluPerspective(45, widthHeightRatio, 1, 1000);
+		glu.gluLookAt(0, 0, 100, 0, 0, 0, 0, 1, 0);
 
-        gl.glMatrixMode(GL.GL_MODELVIEW);
-        gl.glLoadIdentity();
+		gl.glMatrixMode(GL.GL_MODELVIEW);
+		gl.glLoadIdentity();
 	}
 
-	// Método chamado pelo 'animator' a cada 1/60 de segundo para desenhar na tela
+	// Método chamado pelo 'animator' a cada 1/60 de segundo para desenhar na
+	// tela
 	@Override
 	public void display(GLAutoDrawable drawable) {
-		
+
 		GL gl = drawable.getGL();
-        gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
-        
-        setCamera(gl);
-        
-        gl.glColor4f(1f, 1f, 1f, 0.9f);
-        
-        gl.glBegin(GL.GL_TRIANGLE_FAN);
-        gl.glVertex3f(-20, -20, 0);
-        gl.glVertex3f(+20, -20, 0);
-        gl.glVertex3f(0, 20, 0);
-        gl.glEnd();
+		gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+
+		setCamera(gl);
+
+		// Este array terá de ser convertido a partir de this.pontos
+		float[] pontos = new float[] {
+				-20, +20, 0,
+				+20, +20, 0,
+				-10, +20, 0,
+				+10, +10, 0,
+				-20, +10, 0
+		};
+		
+		FloatBuffer pontosBuffer = BufferUtil.newFloatBuffer(pontos.length);
+		pontosBuffer.put(pontos);
+		pontosBuffer.rewind();
+		
+		gl.glColor3f(1f, 1f, 1f);
+		gl.glVertexPointer(3, GL.GL_FLOAT, 0, pontosBuffer);
+		gl.glDrawArrays(GL.GL_POINTS, 0, pontos.length / 3);
 	}
 
 	// Método chamado na inicialização da tela
@@ -69,10 +81,11 @@ public class Tela extends GLCanvas implements GLEventListener {
 		gl.glShadeModel(GL.GL_SMOOTH);
 		gl.glHint(GL.GL_PERSPECTIVE_CORRECTION_HINT, GL.GL_NICEST);
 		gl.glClearColor(0f, 0f, 0f, 1f);
+		gl.glEnableClientState(GL.GL_VERTEX_ARRAY);
 
 		animator = new FPSAnimator(this, FPS);
 		animator.start();
-		
+
 		glu = new GLU();
 	}
 
@@ -83,7 +96,7 @@ public class Tela extends GLCanvas implements GLEventListener {
 		GL gl = drawable.getGL();
 		gl.glViewport(0, 0, width, height);
 	}
-	
+
 	@Override
 	public void displayChanged(GLAutoDrawable arg0, boolean arg1, boolean arg2) {
 		// Faz nada
